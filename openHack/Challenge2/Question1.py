b2d36da1fb56a34a48abda163c41c846fdebe7a1
Julian Lee <julianle@microsoft.com>
2020-01-15 12:49:17 -0800

    Challenge 1

---
+++ openHack/Challenge2/Question1.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "https://kmopenhacksearchservice.search.windows.net/indexes/openhack-jl-index/docs/search?api-version=2019-05-06"
+
+payload = "{  \r\n     \"count\": true,\r\n     \"search\": \"New York\",  \r\n     \"select\": \"file_name, url, size, last_modified\"\r\n}  "
+headers = {
+  'api-key': 'F65343057B0C61194591A35C44D48CD4',
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data = payload)
+
+print(response.text.encode('utf8'))
